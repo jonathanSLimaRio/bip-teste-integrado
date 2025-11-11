6e22908268f8075ddbd1c429620a0a581e237c38
jonathanSLimaRio <jonathan.planeje@gmail.com>
2025-11-11 16:14:06 -0300

    feat: update app structure by refining component imports and fixing styleUrls in app.ts; adjust main.ts to bootstrap App component

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,11 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app/app.routes';
-import { AppComponent } from './app/app.component';
+import { App } from './app/app';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, {
+bootstrapApplication(App, {
   providers: [
     provideHttpClient(),
-    provideRouter(appRoutes),
+    ...(appConfig.providers ?? []),
   ],
 }).catch(err => console.error(err));
